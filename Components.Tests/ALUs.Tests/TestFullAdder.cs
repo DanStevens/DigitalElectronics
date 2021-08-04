a43b23b63d11a8f29c3d4fc0e49ddfa32232f666
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2021-08-04 21:45:29 +0100

    Added full adder

---
+++ Components.Tests/ALUs.Tests/TestFullAdder.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalElectronics.Components.ALUs.Tests
+{
+    public class TestFullAdder
+    {
+        [TestCase(false, false, false, false, false)]
+        [TestCase(false, false, true, false, true)]
+        [TestCase(false, true, false, false, true)]
+        [TestCase(false, true, true, true, false)]
+        [TestCase(true, false, false, false, true)]
+        [TestCase(true, false, true, true, false)]
+        [TestCase(true, true, true, true, true)]
+        public void TestLogic(bool inputA, bool inputB, bool inputC, bool expectedOutputC, bool expectedOutputS)
+        {
+            FullAdder fullAdder = new FullAdder();
+            fullAdder.SetInputA(inputA);
+            fullAdder.SetInputB(inputB);
+            fullAdder.SetInputC(inputC);
+            fullAdder.OutputC.Should().Be(expectedOutputC, nameof(fullAdder.OutputC));
+            fullAdder.OutputS.Should().Be(expectedOutputS, nameof(fullAdder.OutputS));
+        }
+    }
+}
---
+++ Components/ALUs/FullAdder.cs
@@ -0,0 +1,59 @@
+﻿using DigitalElectronics.Components.LogicGates;
+
+namespace DigitalElectronics.Components.ALUs
+{
+
+    /// <summary>
+    /// Represents a full adder for summing a single pair of bits
+    /// </summary>
+    public class FullAdder
+    {
+        private readonly HalfAdder _halfAdder1, _halfAdder2;
+        private readonly OrGate _or;
+
+        public FullAdder()
+        {
+            _halfAdder1 = new HalfAdder();
+            _halfAdder2 = new HalfAdder();
+            _or = new OrGate();
+        }
+
+        /// <summary>
+        /// Sets value for A input
+        /// </summary>
+        public void SetInputA(bool value)
+        {
+            _halfAdder1.SetInputA(value);
+        }
+
+        /// <summary>
+        /// Sets value for B input
+        /// </summary>
+        public void SetInputB(bool value)
+        {
+            _halfAdder1.SetInputB(value);
+        }
+
+        /// <summary>
+        /// Sets value for 'Carry' input
+        /// </summary>
+        public void SetInputC(bool value)
+        {
+            _halfAdder2.SetInputB(value);
+            _halfAdder2.SetInputA(_halfAdder1.OutputS);
+            _or.SetInputA(_halfAdder1.OutputC);
+            _or.SetInputB(_halfAdder2.OutputC);
+        }
+
+        /// <summary>
+        /// Gets state of 'Carry' output
+        /// </summary>
+        public bool OutputC => _or.OutputQ;
+
+        /// <summary>
+        /// Gets state of 'Sum' output
+        /// </summary>
+        public bool OutputS => _halfAdder2.OutputS;
+
+    }
+}
