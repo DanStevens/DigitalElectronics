59877f3cc9b59b14d9ceebac535657a9eb769a8e
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2021-08-04 21:14:34 +0100

    Added half adder component

---
+++ Components.Tests/ALUs.Tests/TestHalfAdder.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalElectronics.Components.ALUs.Tests
+{
+    public class TestHalfAdder
+    {
+        [TestCase(false, false, false, false)]
+        [TestCase(false, true, false, true)]
+        [TestCase(true, false, false, true)]
+        [TestCase(true, true, true, false)]
+        public void TestLogic(bool inputA, bool inputB, bool expectedOutputC, bool expectedOutputS)
+        {
+            HalfAdder halfAdder = new HalfAdder();
+            halfAdder.SetInputA(inputA);
+            halfAdder.SetInputB(inputB);
+            halfAdder.OutputC.Should().Be(expectedOutputC);
+            halfAdder.OutputS.Should().Be(expectedOutputS);
+        }
+    }
+}
---
+++ Components/ALUs/HalfAdder.cs
@@ -0,0 +1,49 @@
+﻿using DigitalElectronics.Components.LogicGates;
+
+namespace DigitalElectronics.Components.ALUs
+{
+
+    /// <summary>
+    /// Represents a half adder for summing a single pair of bits
+    /// </summary>
+    public class HalfAdder
+    {
+        private readonly XorGate _xor;
+        private readonly AndGate _and;
+
+        public HalfAdder()
+        {
+            _xor = new XorGate();
+            _and = new AndGate();
+        }
+
+        /// <summary>
+        /// Sets value for A input
+        /// </summary>
+        public void SetInputA(bool value)
+        {
+            _xor.SetInputA(value);
+            _and.SetInputA(value);
+        }
+
+        /// <summary>
+        /// Sets value for B input
+        /// </summary>
+        public void SetInputB(bool value)
+        {
+            _xor.SetInputB(value);
+            _and.SetInputB(value);
+        }
+
+        /// <summary>
+        /// Gets state of 'Carry' output
+        /// </summary>
+        public bool OutputC => _and.OutputQ;
+
+        /// <summary>
+        /// Gets state of 'Sum' output
+        /// </summary>
+        public bool OutputS => _xor.OutputQ;
+
+    }
+}
