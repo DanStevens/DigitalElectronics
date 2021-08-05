9452b14fbfe6307094ab560f78408a81367a3d3a
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2021-08-05 13:51:01 +0100

    Added tri-state buffer component

---
+++ Components.Tests/LogicGates.Tests/TestTriStateBuffer.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DigitalElectronics.Components.LogicGates.Tests
+{
+    public class TestTriStateBuffer
+    {
+        [TestCase(false, false, null)]
+        [TestCase(true, false, null)]
+        [TestCase(false, true, false)]
+        [TestCase(true, true, true)]
+        public void TestLogic(bool inputA, bool inputB, bool? expectedOutputC)
+        {
+            TriStateBuffer triStateBuffer = new TriStateBuffer();
+            triStateBuffer.SetInputA(inputA);
+            triStateBuffer.SetInputB(inputB);
+            triStateBuffer.OutputC.Should().Be(expectedOutputC);
+        }
+    }
+}
---
+++ Components/LogicGates/TriStateBuffer.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace DigitalElectronics.Components.LogicGates
+{
+    
+    /// <summary>
+    /// Represents a tri-state buffer
+    /// </summary>
+    [DebuggerDisplay("3S Buffer: A = {_inputA}; B = {_inputB}; C = {OutputC}")]
+    public class TriStateBuffer
+    {
+        private bool _inputA;
+        private bool _inputB;
+
+        /// <summary>
+        /// Sets value for A input
+        /// </summary>
+        public void SetInputA(bool value)
+        {
+            _inputA = value;
+        }
+
+        /// <summary>
+        /// Sets value for B input
+        /// </summary>
+        public void SetInputB(bool value)
+        {
+            _inputB = value;
+        }
+
+        /// <summary>
+        /// Gets state of C output, where `null` indicates Z (high impedance) state
+        /// </summary>
+        public bool? OutputC => _inputB ? _inputA : null;
+    }
+}
