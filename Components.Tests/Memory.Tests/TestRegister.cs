366f786c6fc71b3124819d7979cda360b8b36ee2
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2021-08-06 20:18:26 +0100

    Added Register component

---
+++ Components.Tests/Memory.Tests/TestRegister.cs
@@ -0,0 +1,129 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DigitalElectronics.Components.Memory.Tests
+{
+    public class TestRegister
+    {
+        Register _register;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _register = new Register();
+        }
+
+        [Test]
+        public void AHighOutputDoesNotChange_WhenClocking_IfInputLIsLow()
+        {
+            ReleaseL(); AssertOutput(true);
+
+            ReleaseD(); AssertOutput(true);
+            Clock();    AssertOutput(true);
+            PushD();    AssertOutput(true);
+            Clock();    AssertOutput(true);
+
+            ReleaseD(); AssertOutput(true);
+            Clock();    AssertOutput(true);
+            PushD();    AssertOutput(true);
+            Clock();    AssertOutput(true);
+        }
+
+        [Test]
+        public void ALowOutputDoesNotChange_WhenClocking_IfInputLIsLow()
+        {
+            // Get output to go low 
+            PushL();    AssertOutput(true);
+            Clock();    AssertOutput(false);
+            ReleaseL(); AssertOutput(false);
+
+            // Now test
+            ReleaseD(); AssertOutput(false);
+            Clock();    AssertOutput(false);
+            PushD();    AssertOutput(false);
+            Clock();    AssertOutput(false);
+
+            // Repeat test
+            ReleaseD(); AssertOutput(false);
+            Clock();    AssertOutput(false);
+            PushD();    AssertOutput(false);
+            Clock();    AssertOutput(false);
+        }
+
+        [Test]
+        public void OutputDoesChange_WhenClocking_IfInputLIsHigh()
+        {
+            PushL();    AssertOutput(true);
+            Clock();    AssertOutput(false);
+            Clock();    AssertOutput(false);
+
+            PushD();    AssertOutput(false);
+            Clock();    AssertOutput(true);
+            ReleaseD(); AssertOutput(true);
+            Clock();    AssertOutput(false);
+
+            PushD(); AssertOutput(false);
+            Clock(); AssertOutput(true);
+            ReleaseD(); AssertOutput(true);
+            Clock(); AssertOutput(false);
+        }
+
+
+        [Test]
+        public void OutputRises_WhenClocking_IfInputDAndInputLAreHigh()
+        {
+            // Get output to go low 
+            PushL();    AssertOutput(true);
+            Clock();    AssertOutput(false);
+            ReleaseL(); AssertOutput(false);
+
+            PushD();    AssertOutput(false);
+            PushL();    AssertOutput(false);
+            Clock();    AssertOutput(true);
+        }
+
+        [Test]
+        public void OutputFalls_WhenClocking_IfInputDIsLowAndInputLIsHigh()
+        {
+            ReleaseD(); AssertOutput(true);
+            PushL();    AssertOutput(true);
+            Clock();    AssertOutput(false);
+        }
+
+        [Test]
+        public void CombinationTest()
+        {
+            AssertOutput(true);
+
+            PushL(); AssertOutput(true);
+            Clock(); AssertOutput(false);
+            ReleaseL(); AssertOutput(false);
+
+            PushL(); AssertOutput(false);
+            PushD(); AssertOutput(false);
+            Clock(); AssertOutput(true);
+            ReleaseL(); AssertOutput(true);
+
+            Clock(); AssertOutput(true);
+
+            PushL(); AssertOutput(true);
+            Clock(); AssertOutput(true);
+            ReleaseD(); AssertOutput(true);
+            Clock(); AssertOutput(false);
+            ReleaseL(); AssertOutput(false);
+            PushD(); AssertOutput(false);
+            Clock(); AssertOutput(false);
+
+            PushL(); AssertOutput(false);
+            Clock(); AssertOutput(true);
+        }
+
+
+        private void PushL() => _register.SetInputL(true);
+        private void ReleaseL() => _register.SetInputL(false);
+        private void PushD() => _register.SetInputD(true);
+        private void ReleaseD() => _register.SetInputD(false);
+        private void Clock() => _register.Clock();
+        private void AssertOutput(bool v) => _register.OutputQ.Should().Be(v);
+    }
+}
---
+++ Components/Memory/Register.cs
@@ -0,0 +1,74 @@
+﻿using DigitalElectronics.Components.FlipFlops;
+using DigitalElectronics.Components.LogicGates;
+using System.Diagnostics;
+
+namespace DigitalElectronics.Components.Memory
+{
+    /// <summary>
+    /// Represents a single-bit register
+    /// </summary>
+    /// <remarks>
+    /// A register 'latches' in whatever value is on 'Data' input at the point `Clock()` is
+    /// invoked, but only if 'Load' input is high. If 'Load' is low, the currently latched
+    /// value remains latched.
+    /// </remarks>
+    [DebuggerDisplay("Register: Q = {OutputQ}")]
+    public class Register
+    {
+        private DFlipFlop _dFlipFlop;
+        private Inverter _not;
+        private AndGate _and1, _and2;
+        private OrGate _or;
+
+        public Register()
+        {
+            _dFlipFlop = new DFlipFlop();
+            _not = new Inverter();
+            _and1 = new AndGate();
+            _and2 = new AndGate();
+            _or = new OrGate();
+            Sync();
+        }
+
+        /// <summary>
+        /// Sets value for 'Load' input
+        /// </summary>
+        public void SetInputL(bool value)
+        {
+            _not.SetInputA(value);
+            _and2.SetInputA(value);
+            Sync();
+        }
+
+        /// <summary>
+        /// Sets value for 'Data' input
+        /// </summary>
+        public void SetInputD(bool value)
+        {
+            _and2.SetInputB(value);
+            Sync();
+        }
+        /// <summary>
+        /// Gets state of Q output
+        /// </summary>
+        public bool OutputQ => _dFlipFlop.OutputQ;
+
+        /// <summary>
+        /// Simulates the receipt of a clock pulse
+        /// </summary>
+        public void Clock()
+        {
+            _dFlipFlop.Clock();
+        }
+
+        private void Sync()
+        {
+            _and1.SetInputB(_not.OutputQ);
+            _or.SetInputB(_and2.OutputQ);
+            _and1.SetInputA(_dFlipFlop.OutputQ);
+            _or.SetInputA(_and1.OutputQ);
+            _dFlipFlop.SetInputD(_or.OutputQ);
+        }
+
+    }
+}
