d57a6b88cbd8d0635d57c3140c0fb060f94afacc
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2021-08-08 23:30:50 +0100

    Added `NBitRegister` component

---
+++ Components.Tests/Memory.Tests/TestNBitRegister.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DigitalElectronics.Components.Memory.Tests
+{
+    class TestNBitRegister
+    {
+        NBitRegister _4bitRegister;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _4bitRegister = new NBitRegister(4);
+        }
+
+        [Test]
+        public void BitCount_ShouldBe4()
+        {
+            _4bitRegister.BitCount.Should().Be(4);
+        }
+
+        [Test]
+        public void AllOutputsShouldBeHigh_OnInitialization()
+        {
+            AssertOutputs(true, true, true, true);
+        }
+
+        [Test]
+        public void TestLatchInZero()
+        {
+            SetInputsD(false, false, false, false);
+            AssertOutputs(true, true, true, true);
+
+            SetInputL(true);
+            AssertOutputs(true, true, true, true);
+
+            Clock();
+            AssertOutputs(false, false, false, false);
+        }
+
+        [Test]
+        public void TestLatchInOne()
+        {
+            SetInputsD(false, false, false, true);
+            AssertOutputs(true, true, true, true);
+            
+            PushL();
+            AssertOutputs(true, true, true, true);
+
+            Clock();
+            AssertOutputs(false, false, false, true);
+        }
+
+
+        [Test]
+        public void OutputOnlyChanges_WhenClocking_IfInputLIsHigh()
+        {
+            ReleaseL();
+            SetInputsD(false, false, false, false);
+            AssertOutputs(true, true, true, true);
+            Clock();
+            AssertOutputs(true, true, true, true);
+            SetInputsD(false, false, false, true);
+            Clock();
+            AssertOutputs(true, true, true, true);
+
+            PushL();
+            SetInputsD(false, false, true, false);
+            Clock();
+            AssertOutputs(false, false, true, false);
+            SetInputsD(false, false, true, true);
+            Clock();
+            AssertOutputs(false, false, true, true);
+            
+            ReleaseL();
+            SetInputsD(false, true, false, false);
+            Clock();
+            AssertOutputs(false, false, true, true);
+            PushL();
+            Clock();
+            AssertOutputs(false, true, false, false);
+        }
+
+        [Test]
+        public void TestFrom0CountTo15()
+        {
+            for (byte i = 0; i <= 15; i++)
+            {
+                var iBits = new BitArray(new byte[] { i });
+                SetInputsD(iBits);
+                Clock();
+            }
+        }
+
+        private void AssertOutputs(params bool[] expectedOutputs)
+        {
+            for (int x = 0; x < _4bitRegister.BitCount; x++)
+            {
+                _4bitRegister.GetOutputQx(x).Should().Be(expectedOutputs[x]);
+            }
+        }
+
+        void SetInputDx(int x, bool value) => _4bitRegister.SetInputDx(x, value);
+
+        void SetInputsD(params bool[] values)
+        {
+            for (int x = 0; x < values.Length; x++) SetInputDx(x, values[x]);
+        }
+
+        void SetInputsD(BitArray bitArray)
+        {
+            for (int x = 0; x < _4bitRegister.BitCount; x++) SetInputDx(x, bitArray.Get(x));
+        }
+        
+        void SetInputL(bool value) => _4bitRegister.SetInputL(value);
+
+        void PushL() => SetInputL(true);
+
+        void ReleaseL() => SetInputL(false);
+
+        void Clock() => _4bitRegister.Clock();
+    }
+}
---
+++ Components/Memory/NBitRegister.cs
@@ -0,0 +1,62 @@
+﻿namespace DigitalElectronics.Components.Memory
+{
+    /// <summary>
+    /// Represents a multi-bit register of N bits, where each bit has its own input
+    /// </summary>
+    public class NBitRegister
+    {
+
+        private Register[] _registers;
+
+        /// <summary>
+        /// Constructs a multi-bit register with the given number of bits
+        /// </summary>
+        /// <param name="numberOfBits"></param>
+        public NBitRegister(int numberOfBits)
+        {
+            _registers = new Register[numberOfBits];
+            for (int x = 0; x < numberOfBits; x++) _registers[x] = new Register();
+        }
+
+        /// <summary>
+        /// The number of bits in the register (N)
+        /// </summary>
+        public int BitCount => _registers.Length;
+
+        /// <summary>
+        /// Sets value for the input 'Data' for the bit at the given position
+        /// </summary>
+        /// <param name="x">The bit position (zero-based index)</param>
+        public void SetInputDx(int x, bool value)
+        {
+            _registers[x].SetInputD(value);
+        }
+
+        /// <summary>
+        /// Sets value for 'Load' input
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetInputL(bool value)
+        {
+            for (int x = 0; x < BitCount; x++) _registers[x].SetInputL(value);
+        }
+        
+        /// <summary>
+        /// Gets state of Q output for the given bit
+        /// </summary>
+        /// <param name="x">The bit position (zero-based index)</param>
+        /// <returns></returns>
+        public bool GetOutputQx(int x)
+        {
+            return _registers[x].OutputQ;
+        }
+
+        /// <summary>
+        /// Simulates the receipt of a clock pulse
+        /// </summary>
+        public void Clock()
+        {
+            for (int x = 0; x < BitCount; x++) _registers[x].Clock();
+        }
+    }
+}
