bc5fa992866c6b66bbd882d483f291999bc0fa45
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2021-08-13 19:42:25 +0100

    Began building a 'computer', starting with two registers and a simple bus transfer between them

---
+++ Computers.Tests/TestComputer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using DigitalElectronics.Components.Memory;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace DigitalElectronics.Computers.Tests
+{
+    public class TestComputer
+    {
+        private const int NumberOfBits = 8;
+
+        private NBitRegister _registerA;
+        private NBitRegister _registerB;
+        private Computer _computer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _registerA = new NBitRegister(NumberOfBits);
+            _registerB = new NBitRegister(NumberOfBits);
+            _computer = new Computer(_registerA, _registerB);
+        }
+
+        [Test]
+        public void TestBusTransferBetweenTwo8bitRegisters()
+        {
+            BitArray data = new BitArray(new byte[] { 47 });
+
+            _registerA.ProbeState().Should().BeEquivalentTo(new BitArray(NumberOfBits, true));
+            _registerB.ProbeState().Should().BeEquivalentTo(new BitArray(NumberOfBits, true));
+
+            // Load data into register A
+            _registerA.SetInputL(true);
+            _registerA.SetAllInputsD(data);
+             _computer.Clock();
+            _registerA.ProbeState().Should().BeEquivalentTo(data);
+
+            // Enable register A for reading
+            _registerA.SetInputE(true);
+            _registerA.AllOutputs.Should().BeEquivalentTo(data);
+
+            // Load data into register B from register A
+            _registerB.SetInputL(true);
+            _registerB.SetAllInputsD(_registerA.AllOutputs);
+             _computer.Clock();
+            _registerB.ProbeState().Should().BeEquivalentTo(data);
+        }
+    }
+}
---
+++ Computers/Computer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DigitalElectronics.Components.Memory;
+
+namespace DigitalElectronics.Computers
+{
+    public class Computer
+    {
+        private NBitRegister _registerA;
+        private NBitRegister _registerB;
+
+        public Computer(NBitRegister registerA, NBitRegister registerB)
+        {
+            _registerA = registerA ?? throw new ArgumentNullException(nameof(registerA));
+            _registerB = registerB ?? throw new ArgumentNullException(nameof(registerB));
+        }
+
+        public void Clock()
+        {
+            _registerA.Clock();
+            _registerB.Clock();
+        }
+    }
+}
