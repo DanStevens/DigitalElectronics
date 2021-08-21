e9350964e1d161b28accf4e0bf740bffc58594c6
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2021-08-21 13:17:05 +0100

    Added AND logic gate with 3 inputs

---
+++ Components.Tests/LogicGates.Tests/TestTripleInputAndGate.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DigitalElectronics.Components.LogicGates.Tests
+{
+    public class TestTripleInputAndGate
+    {
+        [TestCase(false, false, false, false)]
+        [TestCase(true, false, false, false)]
+        [TestCase(false, true, false, false)]
+        [TestCase(true, true, false, false)]
+        [TestCase(false, false, true, false)]
+        [TestCase(true, false, true, false)]
+        [TestCase(false, true, true, false)]
+        [TestCase(true, true, true, true)]
+        public void TestLogic(bool a, bool b, bool c, bool expectedQ)
+        {
+            var _3inAndGate = new TripleInputAndGate();
+            _3inAndGate.SetInputA(a);
+            _3inAndGate.SetInputB(b);
+            _3inAndGate.SetInputC(c);
+            _3inAndGate.OutputQ.Should().Be(expectedQ);
+        }
+    }
+}
---
+++ Components/LogicGates/TripleInputAndGate.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+namespace DigitalElectronics.Components.LogicGates
+{
+    
+    /// <summary>
+    /// Represents a AND logic gate with 3 inputs
+    /// </summary>
+    [DebuggerDisplay("AND: A = {_and1._inputA}; B = {_and1._inputB}; C = {_and2._inputC}; Q = {OutputQ}")]
+    public class TripleInputAndGate
+    {
+        private AndGate _and1, _and2;
+
+        public TripleInputAndGate()
+        {
+            _and1 = new AndGate();
+            _and2 = new AndGate();
+        }
+
+        public void SetInputA(bool a)
+        {
+            _and1.SetInputA(a);
+            Sync();
+        }
+
+        public void SetInputB(bool b)
+        {
+            _and1.SetInputB(b);
+            Sync();
+        }
+
+        public void SetInputC(bool c)
+        {
+            _and2.SetInputB(c);
+            Sync();
+        }
+
+        public bool OutputQ => _and2.OutputQ;
+
+        private void Sync()
+        {
+            _and2.SetInputA(_and1.OutputQ);
+        }
+    }
+}
