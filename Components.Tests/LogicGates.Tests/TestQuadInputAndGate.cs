0f012d6e8fbc816985792a2a30abc561a5fc4ae9
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2021-08-23 21:37:51 +0100

    Added AND logic gate with 4 inputs

---
+++ Components.Tests/LogicGates.Tests/TestQuadInputAndGate.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DigitalElectronics.Components.LogicGates.Tests
+{
+    public class TestQuadInputAndGate
+    {
+        [TestCase(false, false, false, false, false)]
+        [TestCase(true, false, false, false, false)]
+        [TestCase(false, true, false, false, false)]
+        [TestCase(true, true, false, false, false)]
+        [TestCase(false, false, true, false, false)]
+        [TestCase(true, false, true, false, false)]
+        [TestCase(false, true, true, false, false)]
+        [TestCase(true, true, true, false, false)]
+        [TestCase(false, false, false, true, false)]
+        [TestCase(true, false, false, true, false)]
+        [TestCase(false, true, false, true, false)]
+        [TestCase(true, true, false, true, false)]
+        [TestCase(false, false, true, true, false)]
+        [TestCase(true, false, true, true, false)]
+        [TestCase(false, true, true, true, false)]
+        [TestCase(true, true, true, true, true)]
+        public void TestLogic(bool a, bool b, bool c, bool d, bool expectedQ)
+        {
+            var _4inAndGate = new QuadInputAndGate();
+            _4inAndGate.SetInputA(a);
+            _4inAndGate.SetInputB(b);
+            _4inAndGate.SetInputC(c);
+            _4inAndGate.SetInputD(d);
+            _4inAndGate.OutputQ.Should().Be(expectedQ);
+        }
+    }
+}
---
+++ Components/LogicGates/QuadInputAndGate.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace DigitalElectronics.Components.LogicGates
+{
+    
+    /// <summary>
+    /// Represents a AND logic gate with 4 inputs
+    /// </summary>
+    [DebuggerDisplay("AND: A = {_andAB._inputA}; B = {_andAB._inputB}; C = {_andCD._inputA};  D = {_andCD._inputB}; Q = {OutputQ}")]
+    public class QuadInputAndGate
+    {
+        private AndGate _andAB, _andCD, _andQ;
+
+        public QuadInputAndGate()
+        {
+            _andAB = new AndGate();
+            _andCD = new AndGate();
+            _andQ = new AndGate();
+        }
+
+        public void SetInputA(bool a)
+        {
+            _andAB.SetInputA(a);
+            Sync();
+        }
+
+        public void SetInputB(bool b)
+        {
+            _andAB.SetInputB(b);
+            Sync();
+        }
+
+        public void SetInputC(bool c)
+        {
+            _andCD.SetInputA(c);
+            Sync();
+        }
+
+        public void SetInputD(bool d)
+        {
+            _andCD.SetInputB(d);
+            Sync();
+        }
+
+        public bool OutputQ => _andQ.OutputQ;
+
+        private void Sync()
+        {
+            _andQ.SetInputA(_andAB.OutputQ);
+            _andQ.SetInputB(_andCD.OutputQ);
+        }
+    }
+}
