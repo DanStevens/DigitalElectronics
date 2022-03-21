0478e656dfb4fd49c83071f2e8622be23baac9f8
Dan Stevens <danstevens@users.noreply.github.com>
2022-03-21 19:59:19 +0000

    Created GatedSRLatchDemo view

---
+++ Views/DigitalElectronics.Demos/GatedSRLatchDemo.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DigitalElectronics.Demos
+{
+    /// <summary>
+    /// Interaction logic for GatedSRLatchDemo.xaml
+    /// </summary>
+    public partial class GatedSRLatchDemo : Window
+    {
+        public GatedSRLatchDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void ResetButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _viewModel.Reset = true;
+        }
+
+        private void ResetButton_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _viewModel.Reset = false;
+        }
+
+        private void SetButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _viewModel.Set = true;
+        }
+
+        private void SetButton_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _viewModel.Set = false;
+        }
+    }
+}
