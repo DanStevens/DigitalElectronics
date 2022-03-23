9172d976a64846bbc3bc3b38f014d8975834bd64
Dan Stevens <danstevens@users.noreply.github.com>
2022-03-23 23:59:59 +0000

    Created new utility class `BitArrayComparer` and corresponding test class

---
+++ Common/Tests/Utilities.Tests/BitArrayComparerTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using FluentAssertions;
+using NUnit.Framework;
+
+[assembly: DebuggerDisplay("BitArray={DigitalElectronics.Utilities.Extensions.ToString(this)}", Target = typeof(BitArray))]
+
+namespace DigitalElectronics.Utilities.Tests
+{
+    public class BitArrayComparerTests
+    {
+        [Test]
+        public void Compare_GivenSameObject_ShouldReturnZero()
+        {
+            var bitArray = new BitArray(byte.MaxValue);
+            var comparer = new BitArrayComparer();
+            comparer.Compare(bitArray, bitArray);
+        }
+
+        [Test]
+        public void Compare_NullComparedWithBitArray_ShouldReturnNeg1()
+        {
+            var comparer = new BitArrayComparer();
+            comparer.Compare(new BitArray(0), null).Should().Be(1);
+        }
+
+        [Test]
+        public void Compare_NullComparedWithBitArray_ShouldReturnPos1()
+        {
+            var comparer = new BitArrayComparer();
+            comparer.Compare(null, new BitArray(0)).Should().Be(-1);
+        }
+
+        [Test]
+        public void Compare_BitArrayComparedWithBitArray_ShouldReturnTBD()
+        {
+            var comparer = new BitArrayComparer();
+            var bitConverter = new BitConverter();
+
+            for (byte x = 0; x < 255; x++)
+            {
+                for (byte y = 0; y < 255; y++)
+                {
+                    var bitArrayX = bitConverter.GetBits(x);
+                    var bitArrayY = bitConverter.GetBits(y);
+                    var expected = x.CompareTo(y);
+
+                    var result = comparer.Compare(bitArrayX, bitArrayY);
+                    result.Should().Be(expected);
+                }
+            }
+        }
+
+        [Test]
+        public void BitArrayToByte_ShouldConvertBitArrayBackToByte()
+        {
+            for (byte x = 0; x < 255; x++)
+            {
+                var ba = new BitConverter().GetBits(x);
+                ba.ToByte().Should().Be(x);
+            }
+        }
+    }
+}
---
+++ Common/Utilities/BitArrayComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DigitalElectronics.Utilities
+{
+    public class BitArrayComparer : IComparer<BitArray>
+    {
+        public int Compare(BitArray x, BitArray y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return 0;
+            }
+
+            if (ReferenceEquals(null, y))
+            {
+                return 1;
+            }
+
+            if (ReferenceEquals(null, x))
+            {
+                return -1;
+            }
+
+            return x.ToByte().CompareTo(y.ToByte());
+        }
+    }
+}
