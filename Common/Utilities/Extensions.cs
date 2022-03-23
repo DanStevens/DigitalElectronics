525d9bd8f14c1fead9bc0fa22f3dc72540e4c948
Dan Stevens <danstevens@users.noreply.github.com>
2022-03-23 23:59:15 +0000

    Added missing `this` keyword for parameters in `Extensions.cs`

--- Common/Utilities/Extensions.cs
+++ Common/Utilities/Extensions.cs
@@ -13,9 +13,9 @@ namespace DigitalElectronics.Utilities
         /// <summary>
         /// Returns an enumerable object of bools
         /// </summary>
-        public static IEnumerable<bool> AsEnumerable(BitArray ba)
+        public static IEnumerable<bool> AsEnumerable(this BitArray ba)
         {
-            foreach (bool item in ba)
+            foreach (bool item in ba ?? throw new ArgumentNullException(nameof(ba)))
                 yield return item;
         }
 
@@ -23,9 +23,22 @@ namespace DigitalElectronics.Utilities
         /// Returns the binary representation of the BitArray as a string
         /// </summary>
         /// <returns>A string of '1's or '0's representing each bit of the BitArray</returns>
-        public static string ToString(BitArray ba)
+        public static string ToString(this BitArray ba)
         {
+            _ = ba ?? throw new ArgumentNullException(nameof(ba));
             return string.Join(string.Empty, AsEnumerable(ba).Select(b => b ? "1" : "0"));
+        }
+
+        public static byte ToByte(this BitArray ba)
+        {
+            _ = ba ?? throw new ArgumentNullException(nameof(ba));
+
+            if (ba.Length > 8)
+                throw new ArgumentException("Argument is too long to convert to byte without data loss.");
+
+            byte[] bytes = new byte[1];
+            ba.CopyTo(bytes, 0);
+            return bytes[0];
         }
     }
 }
