4d3ba3be7e7c5c7b8ff1896705e6f3d3eac86268
Dan Stevens <danstevens@users.noreply.github.com>
2022-03-24 21:19:15 +0000

    Moved demos for Components into its own folder

---
+++ Views/DigitalElectronics.Demos/Components/DFlipFlopDemo.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace DigitalElectronics.Demos.Components
+{
+    /// <summary>
+    /// Interaction logic for GatedSRLatchDemo.xaml
+    /// </summary>
+    public partial class DFlipFlopDemo : Window
+    {
+        public DFlipFlopDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void ClockButton_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Clock();
+        }
+    }
+}
---
+++ Views/DigitalElectronics.Demos/Components/GatedDLatchDemo.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace DigitalElectronics.Demos.Components
+{
+    /// <summary>
+    /// Interaction logic for GatedSRLatchDemo.xaml
+    /// </summary>
+    public partial class GatedDLatchDemo : Window
+    {
+        public GatedDLatchDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
