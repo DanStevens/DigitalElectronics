329ff28502751f9cf9903213b94e7481edb0ff4b
Dan Stevens <danstevens@users.noreply.github.com>
2022-03-25 13:45:04 +0000

    Created `LED` user control

---
+++ Views/DigitalElectronics.Demos/Controls/LED.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DigitalElectronics.Demos.Controls
+{
+    /// <summary>
+    /// Interaction logic for LED.xaml
+    /// </summary>
+    public partial class LED : UserControl
+    {
+        public LED()
+        {
+            InitializeComponent();
+            _layoutRoot.DataContext = this;
+        }
+
+        public bool IsOn
+        {
+            get { return (bool)GetValue(IsOnProperty); }
+            set { SetValue(IsOnProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsOn.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsOnProperty =
+            DependencyProperty.Register("IsOn", typeof(bool), typeof(LED), new PropertyMetadata(false));
+
+        public Stroke Stroke
+        {
+            get { return (Stroke)GetValue(StrokeProperty); }
+            set { SetValue(StrokeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StrokeProperty =
+            DependencyProperty.Register("Stroke", typeof(Brush), typeof(LED), new PropertyMetadata(Brushes.Black));
+
+        public Color OffColor
+        {
+            get { return (Color)GetValue(OffColorProperty); }
+            set { SetValue(OffColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Color.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OffColorProperty =
+            DependencyProperty.Register("OffColor", typeof(Color), typeof(LED), new PropertyMetadata(Colors.Transparent));
+
+        public Color OnColor
+        {
+            get { return (Color)GetValue(OnColorProperty); }
+            set { SetValue(OnColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for color.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OnColorProperty =
+            DependencyProperty.Register("OnColor", typeof(Color), typeof(LED), new PropertyMetadata(Colors.OrangeRed));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(LED), new PropertyMetadata(string.Empty));
+
+
+    }
+}
