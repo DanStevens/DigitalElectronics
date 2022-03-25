a038b01f0fad7079cdb2ef0fffbcba5dc7879091
Dan Stevens <danstevens@users.noreply.github.com>
2022-03-25 00:15:02 +0000

    Created new window to demo 8-bit register. So far got data to load upon clock and update probe 'LEDs'

--- Views/DigitalElectronics.Demos/App.xaml.cs
+++ Views/DigitalElectronics.Demos/App.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+
+[assembly: DebuggerDisplay("BitArray={DigitalElectronics.Utilities.Extensions.ToString(this)}", Target = typeof(BitArray))]
 
 namespace DigitalElectronics.Demos
 {
