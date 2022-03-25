dbf81a5a2af9b722fc90cbd0d04a918046ea851e
Dan Stevens <danstevens@users.noreply.github.com>
2022-03-25 15:31:36 +0000

    Created new converter `NullableBoolToBoolConverter` that converts a `bool?` to non-nullable Boolean"/>, where `null` is converted to `false`.
    Updated `OneBitRegisterDemo` window to use `LED` user control and `NullableBoolToBoolConverter`

---
+++ Views/DigitalElectronics.UI/Converters/NullableBoolToBoolConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace DigitalElectronics.Demos.Converters
+{
+
+    /// <summary>
+    /// Converts a <see cref="Nullable{Boolean}"/> to non-nullable <see cref="Boolean"/>,
+    /// where `null` is converted to `false`
+    /// </summary>
+    [ValueConversion(typeof(bool?), typeof(bool))]
+    public class NullableBoolToBoolConverter : MarkupExtension, IValueConverter
+    {
+        private static Lazy<NullableBoolToBoolConverter> instance = new ();
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return instance.Value;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool && (bool)value;
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Views/Tests/DigitalElectronics.UI.Tests/Converters/NullableToBoolConverterTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+using DigitalElectronics.Demos.Converters;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DigitalElectronics.UI.Tests.Converters
+{
+    public class Tests
+    {
+
+        [Test]
+        public void Convert_ShouldReturnBoolOfTrue_WhenGivenNullableBoolOfTrue()
+        {
+            var objUT = new NullableBoolToBoolConverter();
+            var result = objUT.Convert((bool?)true, typeof(bool?), null, CultureInfo.InvariantCulture);
+            result.Should().BeOfType<bool>();
+            result.Should().Be(true);
+        }
+
+        [Test]
+        public void Convert_ShouldReturnBoolOfFalse_WhenGivenNullableBoolOfFalse()
+        {
+            var objUT = new NullableBoolToBoolConverter();
+            var result = objUT.Convert((bool?)false, typeof(bool?), null, CultureInfo.InvariantCulture);
+            result.Should().BeOfType<bool>();
+            result.Should().Be(false);
+        }
+
+        [Test]
+        public void Convert_ShouldReturnBoolOfFalse_WhenGivenNullableBoolOfNull()
+        {
+            var objUT = new NullableBoolToBoolConverter();
+            var result = objUT.Convert((bool?)null, typeof(bool?), null, CultureInfo.InvariantCulture);
+            result.Should().BeOfType<bool>();
+            result.Should().Be(false);
+        }
+
+        [Test]
+        public void Convert_ShouldReturnBoolOfTrue_WhenGivenTrue()
+        {
+            var objUT = new NullableBoolToBoolConverter();
+            var result = objUT.Convert(true, typeof(bool), null, CultureInfo.InvariantCulture);
+            result.Should().BeOfType<bool>();
+            result.Should().Be(true);
+        }
+
+        [Test]
+        public void Convert_ShouldReturnBoolOfFalse_WhenGivenFalse()
+        {
+            var objUT = new NullableBoolToBoolConverter();
+            var result = objUT.Convert(false, typeof(bool), null, CultureInfo.InvariantCulture);
+            result.Should().BeOfType<bool>();
+            result.Should().Be(false);
+        }
+    }
+}
