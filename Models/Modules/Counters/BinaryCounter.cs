bd031224fd4189fd1f03311f428a2b63358f9f2c
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2022-04-23 23:34:11 +0100

    Implemented a multi-bit binary counter

---
+++ Models/Modules/Counters/BinaryCounter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using DigitalElectronics.Components.FlipFlops;
+using DigitalElectronics.Concepts;
+
+namespace DigitalElectronics.Modules.Counters
+{
+    /// <summary>
+    /// Models a multi-bit binary counter of N bits
+    /// </summary>
+    [DebuggerDisplay("Binary Counter: {Output}")]
+    public class BinaryCounter
+    {
+        private readonly JKFlipFlop[] _jkFlipFlops;
+
+        /// <summary>
+        /// Constructs a multi-bit binary counter with the given number of bits
+        /// </summary>
+        /// <param name="numberOfBits">The size of the binary counter in bits</param>
+        public BinaryCounter(int numberOfBits)
+        {
+            if (numberOfBits <= 0)
+                throw new ArgumentOutOfRangeException(nameof(numberOfBits), "Argument must be greater than 0");
+
+            NumberOfBits = numberOfBits;
+            
+            _jkFlipFlops = new JKFlipFlop[NumberOfBits];
+            for (int i = 0; i < NumberOfBits; i++)
+                _jkFlipFlops[i] = CreateJKFlipFlop();
+
+            JKFlipFlop CreateJKFlipFlop()
+            {
+                var jkFlipFlop = new JKFlipFlop();
+                jkFlipFlop.SetInputJ(true);
+                jkFlipFlop.SetInputK(true);
+                return jkFlipFlop;
+            }
+        }
+        
+        /// <summary>
+        /// The size of (number of bits in) the binary counter
+        /// </summary>
+        public int NumberOfBits { get; }
+
+        /// <summary>
+        /// The output of the binary counter
+        /// </summary>
+        public BitArray Output => new (_jkFlipFlops.Select(_ => _.OutputQ));
+
+        /// <summary>
+        /// Simulates the receipt of a clock pulse
+        /// </summary>
+        public void Clock()
+        {
+            for (int i = 0; i < NumberOfBits; i++)
+            {
+                _jkFlipFlops[i].Clock();
+                if (_jkFlipFlops[i].OutputQ)
+                    break;
+            }
+        }
+    }
+}
---
+++ Models/Tests/Modules.Tests/Counters.Tests/TestBinaryCounter.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using DigitalElectronics.Concepts;
+using DigitalElectronics.Modules.Counters;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DigitalElectronics.Modules.Tests.Counters.Tests
+{
+    [TestFixture]
+    public class TestBinaryCounter
+    {
+        [Test]
+        public void InitialState_4Bits()
+        {
+            var fourBitBinaryCounter = new BinaryCounter(4);
+            fourBitBinaryCounter.NumberOfBits.Should().Be(4);
+            fourBitBinaryCounter.Output.ToByte().Should().Be(15);
+            fourBitBinaryCounter.Output.Length.Should().Be(4);
+        }
+
+        [Test]
+        public void InitialState_8Bits()
+        {
+            var fourBitBinaryCounter = new BinaryCounter(8);
+            fourBitBinaryCounter.NumberOfBits.Should().Be(8);
+            fourBitBinaryCounter.Output.ToByte().Should().Be(255);
+            fourBitBinaryCounter.Output.Length.Should().Be(8);
+        }
+
+
+        [Test]
+        public void Test4BitCounter()
+        {
+            var fourBitBinaryCounter = new BinaryCounter(4);
+
+            foreach (int i in Enumerable.Range(0, 16))
+            {
+                fourBitBinaryCounter.Clock();
+                fourBitBinaryCounter.Output.ToByte().Should().Be((byte)i);
+                fourBitBinaryCounter.Output.Length.Should().Be(4);
+            }
+        }
+
+        [Test]
+        public void Test8BitCounter()
+        {
+            var fourBitBinaryCounter = new BinaryCounter(8);
+
+            foreach (int i in Enumerable.Range(0, 256))
+            {
+                fourBitBinaryCounter.Clock();
+                fourBitBinaryCounter.Output.ToByte().Should().Be((byte)i);
+                fourBitBinaryCounter.Output.Length.Should().Be(8);
+            }
+        }
+    }
+}
