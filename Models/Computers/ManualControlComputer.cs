4fdd480b8b954a0243aea3208d960eeeffec20ad
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2022-04-29 23:20:54 +0100

    Created first iteration of a bare-bones 8-bit manual control computer with a test of a simple program that adds 30 and 12 and writes the result to the output register.

---
+++ Models/Computers/ManualControlComputer.cs
@@ -0,0 +1,52 @@
+﻿using DigitalElectronics.Components.Memory;
+using DigitalElectronics.Modules;
+using DigitalElectronics.Modules.ALUs;
+using DigitalElectronics.Modules.Comms;
+using DigitalElectronics.Modules.Memory;
+
+namespace DigitalElectronics.Computers
+{
+
+    /// <summary>
+    /// A bare-bones 8-bit manual control computer, meaning it doesn't have a control logic module.
+    /// The operator must manually enable/disable module input/output and advance the clock.
+    /// </summary>
+    public class ManualControlComputer
+    {
+        private const int WordSize = 8;
+
+        private readonly ArithmeticLogicUnit _alu;
+        private readonly Register _aRegister;
+        private readonly Register _bRegister;
+        private readonly Register _outRegister;
+
+        public ParallelBus Bus { get; }
+        public IRegister ARegister => _aRegister;
+        public IRegister BRegister => _bRegister;
+        public IArithmeticLogicUnit ALU => _alu;
+        public IRAM RAM { get; }
+        ////private IRegister AddressRegister { get; }
+        ////private IRegister InstructionRegister { get; }
+        public IRegister OutRegister => _outRegister;
+
+        public ManualControlComputer()
+        {
+            RAM = new SixteenByteRAM();
+            _aRegister = new Register(WordSize);
+            _bRegister = new Register(WordSize);
+            _alu = new ArithmeticLogicUnit(WordSize);
+            _outRegister = new Register(WordSize);
+
+            Bus = new ParallelBus(WordSize,
+                RAM, _aRegister, _bRegister, _alu, _outRegister);
+        }
+
+        public void Clock()
+        {
+            RAM.Clock();
+            ARegister.Clock();
+            BRegister.Clock();
+            OutRegister.Clock();
+        }
+    }
+}
---
+++ Models/Tests/Computers.Tests/TestManualControlComputer.cs
@@ -0,0 +1,122 @@
+﻿using DigitalElectronics.Concepts;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DigitalElectronics.Computers.Tests
+{
+
+    /// <summary>
+    /// Tests for the 8-bit manual control computer
+    /// </summary>
+    public class TestManualControlComputer
+    {
+        private readonly BitArray Address0x0 = new BitArray((byte)0x0).Trim(4);
+        private readonly BitArray Address0x1 = new BitArray((byte)0x1).Trim(4);
+
+        [Test]
+        public void Create()
+        {
+            var computer = new ManualControlComputer();
+            computer.Should().NotBeNull();
+        }
+
+        [Test]
+        public void OperateComputerToAdd30And12AndOutput()
+        {
+            var computer = new ManualControlComputer();
+
+            // Initialize RAM
+            InitializeRAM();
+
+            // LDA 0x0: Load the contents of memory address 0x0 (0) in to A Register
+            LDA(Address0x0);
+
+            computer.ARegister.ProbeState().ToByte().Should().Be(30);
+            computer.BRegister.ProbeState().ToByte().Should().Be(255);
+
+            // ADD 0x1: Add the contents of memory address 0x1 (1) to the A Register
+            ADD(Address0x1);
+
+            computer.BRegister.ProbeState().ToByte().Should().Be(12);
+            computer.ARegister.ProbeState().ToByte().Should().Be(42);
+
+            // OUT   : Put the contents of A Register into the Output Register
+            OUT();
+
+            computer.OutRegister.ProbeState().ToByte().Should().Be(42);
+
+            void InitializeRAM()
+            {
+                computer.RAM.SetInputL(true);
+
+                computer.RAM.SetInputA(Address0x0);
+                computer.RAM.SetInputD(new BitArray((byte) 30));
+                computer.Clock();
+                computer.RAM.ProbeState(Address0x0).ToByte().Should().Be(30);
+
+                computer.RAM.SetInputA(Address0x1);
+                computer.RAM.SetInputD(new BitArray((byte) 12));
+                computer.RAM.Clock();
+                computer.RAM.ProbeState(Address0x1).ToByte().Should().Be(12);
+
+                computer.RAM.SetInputL(false);
+            }
+
+            void LDA(BitArray address)
+            {
+                computer.RAM.SetInputA(address);
+                computer.RAM.SetInputE(true);
+                
+                computer.ARegister.SetInputL(true);
+                computer.ARegister.SetInputD(computer.Bus.Output);
+                computer.Clock();
+                computer.ALU.SetInputA(computer.ARegister.ProbeState());
+                computer.ARegister.SetInputL(false);
+
+                computer.RAM.SetInputE(false);
+            }
+
+            void ADD(BitArray address)
+            {
+                LDB(address);
+
+                computer.ALU.SetInputEO(true);
+
+                computer.ARegister.SetInputL(true);
+                computer.ARegister.SetInputD(computer.Bus.Output);
+                computer.Clock();
+                computer.ARegister.SetInputL(false);
+
+                computer.ALU.SetInputEO(false);
+            }
+
+            void LDB(BitArray address)
+            {
+                computer.RAM.SetInputA(address);
+                computer.RAM.SetInputE(true);
+
+                computer.BRegister.SetInputL(true);
+                computer.BRegister.SetInputD(computer.Bus.Output);
+                computer.Clock();
+                computer.BRegister.SetInputL(false);
+                computer.ALU.SetInputB(computer.BRegister.ProbeState());
+
+                computer.RAM.SetInputE(false);
+            }
+
+            void OUT()
+            {
+                computer.ARegister.SetInputE(true);
+
+                computer.OutRegister.SetInputL(true);
+                computer.OutRegister.SetInputD(computer.Bus.Output);
+                computer.Clock();
+                computer.OutRegister.SetInputL(false);
+
+                computer.ARegister.SetInputE(false);
+            }
+        }
+
+
+    }
+}
