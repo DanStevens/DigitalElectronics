f23ccd5e9a6402f97bc7f9548b5fe76f8f81fa19
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2022-05-23 16:11:12 +0100

    Updated metadata and categories for dependency properties

--- Views/DigitalElectronics.UI/Controls/SevenSegmentDigit.xaml.cs
+++ Views/DigitalElectronics.UI/Controls/SevenSegmentDigit.xaml.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using DP = System.Windows.DependencyProperty;
+using DPMetadata = System.Windows.FrameworkPropertyMetadata;
+using DPMetadataOptions = System.Windows.FrameworkPropertyMetadataOptions;
 
 namespace DigitalElectronics.UI.Controls
 {
@@ -22,54 +25,73 @@ namespace DigitalElectronics.UI.Controls
 
         #region Lines dependency property
 
+        [Category("Common")]
         public ICollection<bool> Lines
         {
             get { return (ICollection<bool>)GetValue(LinesProperty); }
             set { SetValue(LinesProperty, value); }
         }
 
-        public static readonly DependencyProperty LinesProperty =
-            DependencyProperty.Register("Lines", typeof(ICollection<bool>), typeof(SevenSegmentDigit),
-                new PropertyMetadata(                    LinesDefault));
+        public static readonly DP LinesProperty =
+            DP.Register(nameof(Lines),
+                typeof(ICollection<bool>),
+                typeof(SevenSegmentDigit),
+                new DPMetadata(LinesDefault,
+                    DPMetadataOptions.AffectsRender | DPMetadataOptions.SubPropertiesDoNotAffectRender));
 
         #endregion
 
         #region Stroke dependency property
 
+        [Category("Brush")]
         public Brush Stroke
         {
             get { return (Brush)GetValue(StrokeProperty); }
             set { SetValue(StrokeProperty, value); }
         }
 
-        public static readonly DependencyProperty StrokeProperty =
-            DependencyProperty.Register("Stroke", typeof(Brush), typeof(SevenSegmentDigit), new PropertyMetadata(default));
+        public static readonly DP StrokeProperty =
+            DP.Register(nameof(Stroke),
+                typeof(Brush),
+                typeof(SevenSegmentDigit),
+                new DPMetadata(default,
+                    DPMetadataOptions.AffectsRender | DPMetadataOptions.SubPropertiesDoNotAffectRender));
 
         #endregion
 
         #region SegmentsUnlitColor dependency property
 
+        [Category("Brush")]
         public Color SegmentsUnlitColor
         {
             get { return (Color)GetValue(SegmentsUnlitColorProperty); }
             set { SetValue(SegmentsUnlitColorProperty, value); }
         }
 
-        public static readonly DependencyProperty SegmentsUnlitColorProperty =
-            DependencyProperty.Register("SegmentsUnlitColor", typeof(Color), typeof(SevenSegmentDigit), new PropertyMetadata(Colors.Transparent));
+        public static readonly DP SegmentsUnlitColorProperty =
+            DP.Register(nameof(SegmentsUnlitColor),
+                typeof(Color),
+                typeof(SevenSegmentDigit),
+                new DPMetadata(Colors.Transparent,
+                    DPMetadataOptions.AffectsRender | DPMetadataOptions.SubPropertiesDoNotAffectRender));
 
         #endregion
 
         #region SegmentsLitColor dependency property
 
+        [Category("Brush")]
         public Color SegmentsLitColor
         {
             get { return (Color)GetValue(SegmentsLitColorProperty); }
             set { SetValue(SegmentsLitColorProperty, value); }
         }
 
-        public static readonly DependencyProperty SegmentsLitColorProperty =
-            DependencyProperty.Register("SegmentsLitColor", typeof(Color), typeof(SevenSegmentDigit), new PropertyMetadata(Colors.OrangeRed));
+        public static readonly DP SegmentsLitColorProperty =
+            DP.Register(nameof(SegmentsLitColor),
+                typeof(Color),
+                typeof(SevenSegmentDigit),
+                new DPMetadata(Colors.OrangeRed,
+                    DPMetadataOptions.AffectsRender | DPMetadataOptions.SubPropertiesDoNotAffectRender));
 
         #endregion
 
