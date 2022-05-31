f4bba405adc0fee722271f586ecacb4c3309f806
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2022-05-31 17:38:21 +0100

    Created initial version of `ByteTo3DigitMultiplexedDisplayDecoder` class

---
+++ Models/Modules/Output/ByteTo3DigitMultiplexedDisplayDecoder.cs
@@ -0,0 +1,30 @@
+﻿using DigitalElectronics.Concepts;
+using DigitalElectronics.Modules.Memory;
+
+namespace DigitalElectronics.Modules.Output
+{
+
+    /// <summary>
+    /// Models an unsigned byte to 3 digit decimal multiplexed display decoder
+    /// </summary>
+    /// <remarks>This module decodes a unsigned 8-bit binary input, into a 4 digit
+    /// decimal for use with a multiplexed 7-segment display. Multiplexed means
+    /// only outputs the segment lines for one of the four digits at a time,
+    /// and cycles between them upon each call to <see cref="Clock()"/></remarks>
+    public class ByteTo3DigitMultiplexedDisplayDecoder
+    {
+        private ROM _decoderROM = new (new byte[] { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F });
+
+        public ByteTo3DigitMultiplexedDisplayDecoder()
+        {
+            _decoderROM.SetInputE(true);
+        }
+
+        public void SetInput(BitArray lines)
+        {
+            _decoderROM.SetInputA(lines);
+        }
+
+        public BitArray Output => _decoderROM.Output;
+    }
+}
---
+++ Models/Tests/Modules.Tests/Output.Tests/ByteTo3DigitMultiplexedDisplayDecoderTests.cs
@@ -0,0 +1,25 @@
+﻿using DigitalElectronics.Concepts;
+using DigitalElectronics.Modules.Output;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DigitalElectronics.Modules.Tests.Output.Tests
+{
+    
+    /// <summary>
+    /// Tests for 3 digit signed/unsigned byte multiplexed display decoder
+    /// </summary>
+    public class ByteTo3DigitMultiplexedDisplayDecoderTests
+    {
+        [TestCase(0, 0x3F)]
+        [TestCase(1, 0x06)]
+        [TestCase(2, 0x5B)]
+        [TestCase(9, 0x6F)]
+        public void Output_ShouldBeSetForCC7SegmentDigit_WhenInputIsSetToValue(byte input, byte expectedOutput)
+        {
+            var objUT = new ByteTo3DigitMultiplexedDisplayDecoder();
+            objUT.SetInput(new BitArray(input));
+            objUT.Output.ToByte().Should().Be(expectedOutput);
+        }
+    }
+}
