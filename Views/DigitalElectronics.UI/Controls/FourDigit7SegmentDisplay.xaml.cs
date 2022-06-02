2ba8e9c4e45d89e4eee2c6fbfd32a5a5ee88d8b1
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2022-06-02 22:25:36 +0100

    Created `FourDigit7SegmentDisplay` user control

---
+++ Views/DigitalElectronics.UI/Controls/FourDigit7SegmentDisplay.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DigitalElectronics.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for FourDigit7SegmentDisplay.xaml
+    /// </summary>
+    public partial class FourDigit7SegmentDisplay : UserControl
+    {
+        public FourDigit7SegmentDisplay()
+        {
+            InitializeComponent();
+            _layoutRoot.DataContext = this;
+        }
+
+        #region DigitPadding dependency property
+
+        /// <summary>
+        /// Padding for all digits in the display
+        /// </summary>
+        public Thickness DigitPadding
+        {
+            get => (Thickness)GetValue(DigitPaddingProperty);
+            set => SetValue(DigitPaddingProperty, value);
+        }
+
+        public static readonly DependencyProperty DigitPaddingProperty = DependencyProperty.Register(
+            name: nameof(DigitPadding),
+            propertyType: typeof(Thickness),
+            ownerType: typeof(FourDigit7SegmentDisplay),
+            new PropertyMetadata(new Thickness(5)));
+
+        #endregion
+    }
+}
