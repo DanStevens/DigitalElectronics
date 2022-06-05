b888c65cc0ae7f51504cb2518144f7761afb7b41
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2022-06-05 22:19:12 +0100

    Added properties for specifying input lines for each digit.

--- Views/DigitalElectronics.UI/Controls/FourDigit7SegmentDisplay.xaml.cs
+++ Views/DigitalElectronics.UI/Controls/FourDigit7SegmentDisplay.xaml.cs
@@ -19,8 +19,9 @@ using DPMetadataOptions = System.Windows.FrameworkPropertyMetadataOptions;
 namespace DigitalElectronics.UI.Controls
 {
     /// <summary>
-    /// Interaction logic for FourDigit7SegmentDisplay.xaml
+    /// A 4 digit 7 segment display.
     /// </summary>
+    /// <note>Digits are indexed from 0, starting with the rightmost digit.</note>
     public partial class FourDigit7SegmentDisplay : UserControl
     {
         public FourDigit7SegmentDisplay()
@@ -48,25 +49,79 @@ namespace DigitalElectronics.UI.Controls
 
         #endregion
 
+        #region LinesForSegment0 dependency property
 
-        #region Value dependency property
-
-        public ICollection<bool> Value
+        /// <summary>
+        /// Input lines for the rightmost 7 segment digit
+        /// </summary>
+        public ICollection<bool> LinesForDigit0
         {
-            get => (ICollection<bool>)GetValue(ValueProperty);
-            set => SetValue(ValueProperty, value);
+            get => (ICollection<bool>)GetValue(LinesForDigit0Property);
+            set => SetValue(LinesForDigit0Property, value);
         }
 
-        public static readonly DP ValueProperty = DP.Register(
-            name: nameof(Value),
+        public static readonly DP LinesForDigit0Property = DP.Register(
+            name: nameof(LinesForDigit0),
             propertyType: typeof(ICollection<bool>),
             ownerType: typeof(FourDigit7SegmentDisplay),
-            new DPMetadata(default(ICollection<bool>), ValuePropertyChanged));
+            new DPMetadata(default(ICollection<bool>)));
 
-        private static void ValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        #endregion
+
+        #region LinesForSegment1 dependency property
+
+        /// <summary>
+        /// Input lines for the second digit from the right
+        /// </summary>
+        public ICollection<bool> LinesForDigit1
         {
-            // TODO optional
+            get => (ICollection<bool>)GetValue(LinesForDigit1Property);
+            set => SetValue(LinesForDigit1Property, value);
         }
+
+        public static readonly DP LinesForDigit1Property = DP.Register(
+            name: nameof(LinesForDigit1),
+            propertyType: typeof(ICollection<bool>),
+            ownerType: typeof(FourDigit7SegmentDisplay),
+            new DPMetadata(default(ICollection<bool>)));
+
+        #endregion
+
+        #region LinesForSegment2 dependency property
+
+        /// <summary>
+        /// Input lines for the third digit from the right
+        /// </summary>
+        public ICollection<bool> LinesForDigit2
+        {
+            get => (ICollection<bool>)GetValue(LinesForDigit2Property);
+            set => SetValue(LinesForDigit2Property, value);
+        }
+
+        public static readonly DP LinesForDigit2Property = DP.Register(
+            name: nameof(LinesForDigit2),
+            propertyType: typeof(ICollection<bool>),
+            ownerType: typeof(FourDigit7SegmentDisplay),
+            new DPMetadata(default(ICollection<bool>)));
+
+        #endregion
+
+        #region LinesForSegment3 dependency property
+
+        /// <summary>
+        /// Input lines for the fourth digit from the right
+        /// </summary>
+        public ICollection<bool> LinesForDigit3
+        {
+            get => (ICollection<bool>)GetValue(LinesForDigit3Property);
+            set => SetValue(LinesForDigit3Property, value);
+        }
+
+        public static readonly DP LinesForDigit3Property = DP.Register(
+            name: nameof(LinesForDigit3),
+            propertyType: typeof(ICollection<bool>),
+            ownerType: typeof(FourDigit7SegmentDisplay),
+            new DPMetadata(default(ICollection<bool>)));
 
         #endregion
     }
