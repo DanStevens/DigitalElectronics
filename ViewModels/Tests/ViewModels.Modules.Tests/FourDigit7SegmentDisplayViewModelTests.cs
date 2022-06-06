dcdd2eed16347ac6f6bf7e59555a8b4406213e99
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2022-06-06 22:10:06 +0100

    Got the 4 Digit 7 Segment Display demo to show the 1s digit of the decimal value as specified by the DIP switches

--- ViewModels/Tests/ViewModels.Modules.Tests/FourDigit7SegmentDisplayViewModelTests.cs
+++ ViewModels/Tests/ViewModels.Modules.Tests/FourDigit7SegmentDisplayViewModelTests.cs
@@ -1,4 +1,9 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using DigitalElectronics.Concepts;
+using DigitalElectronics.Utilities;
+using DigitalElectronics.ViewModels.Utilities;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace DigitalElectronics.ViewModels.Modules.Tests
@@ -6,10 +11,33 @@ namespace DigitalElectronics.ViewModels.Modules.Tests
     public class FourDigit7SegmentDisplayViewModelTests
     {
         [Test]
-        public void DoNothing()
+        public void Creation()
         {
             var objUT = new FourDigit7SegmentDisplayViewModel();
             objUT.Should().NotBeNull();
+        }
+
+        [Test]
+        public void InitialState()
+        {
+            var objUT = new FourDigit7SegmentDisplayViewModel();
+
+            objUT.Value.Should().BeOfType<FullyObservableCollection<Bit>>();
+            objUT.Value.Count.Should().Be(8);
+            objUT.Value.ToBitArray().ToByte().Should().Be(0);
+
+            objUT.Lines.Should().BeAssignableTo<IList<bool>>();
+            objUT.Lines.ToBitArray().ToByte().Should().Be(0x3F); // '0' digit
+        }
+
+        [Test]
+        public void Lines()
+        {
+            var objUT = new FourDigit7SegmentDisplayViewModel();
+            using var monitor = objUT.Monitor();
+            objUT.Value[0].Value = true;
+            objUT.Lines.ToBitArray().ToByte().Should().Be(0x06); // '1' digit 
+            monitor.Should().RaisePropertyChangeFor(_ => _.Lines);
         }
     }
 }
--- ViewModels/ViewModels.Modules/ViewModels.Modules/FourDigit7SegmentDisplayViewModel.cs
+++ ViewModels/ViewModels.Modules/ViewModels.Modules/FourDigit7SegmentDisplayViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using DigitalElectronics.Concepts;
 using DigitalElectronics.Modules.Output;
 using DigitalElectronics.ViewModels.Modules.Annotations;
+using DigitalElectronics.ViewModels.Utilities;
 
 namespace DigitalElectronics.ViewModels.Modules
 {
@@ -10,20 +12,22 @@ namespace DigitalElectronics.ViewModels.Modules
     {
         private readonly ByteTo4DigitMultiplexedDisplayDecoder _displayDecoder = new();
 
-        private ObservableCollection<bool> bools;
-
-        public ObservableCollection<bool> Value
+        public FourDigit7SegmentDisplayViewModel()
         {
-            get => bools;
-            set
-            {
-                if (bools != value)
-                {
-                    bools = value;
-                    RaisePropertyChanged(nameof(Value));
-                }
-            }
+            var initialValue = new BitArray((byte)0);
+            Value = new FullyObservableCollection<Bit>(initialValue.Select(b => new Bit(b)));
+            Value.ItemPropertyChanged += OnValueBitChanged;
         }
+
+        private void OnValueBitChanged(object? sender, ItemPropertyChangedEventArgs e)
+        {
+            _displayDecoder.SetInput(new BitArray(Value.ToArray()));
+            RaisePropertyChanged(nameof(Lines));
+        }
+
+        public FullyObservableCollection<Bit> Value { get; }
+
+        public IList<bool> Lines => _displayDecoder.Output.ToList();
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
