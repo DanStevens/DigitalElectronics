7e27898624d513c37dea7ca609b3ab7a2b8b32b3
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2022-09-25 12:32:57 +0100

    WIP performance

--- Devices/BenEater/BenEater.Computers.ViewModels/BE801ComputerViewModel.cs
+++ Devices/BenEater/BenEater.Computers.ViewModels/BE801ComputerViewModel.cs
@@ -9,22 +9,12 @@ namespace DigitalElectronics.BenEater.Computers.ViewModels
         private readonly BE801Computer _computer;
         
         public ClockModuleViewModel ClockModule { get; }
-        public BusViewModel BusModule { get; }
-        public ProgramCounterViewModel ProgramCounterModule { get; }
-        public MemoryModuleViewModel MemoryModule { get; }
-        public ALUViewModel ALUModule { get; }
-        public InstrRegisterViewModel InstrRegisterModule { get; }
         public OutputModuleViewModel OutputModule { get; }
 
         public BE801ComputerViewModel()
         {
             _computer = new();
             ClockModule = new();
-            BusModule = new(_computer);
-            ProgramCounterModule = new(_computer);
-            MemoryModule = new(_computer);
-            ALUModule = new(_computer);
-            InstrRegisterModule = new(_computer);
             OutputModule = new(_computer);
         }
 
@@ -32,11 +22,6 @@ namespace DigitalElectronics.BenEater.Computers.ViewModels
         {
             _computer.Clock();
             ClockModule.Clock();
-            BusModule.Clock();
-            ProgramCounterModule.Clock();
-            MemoryModule.Clock();
-            ALUModule.Clock();
-            InstrRegisterModule.Clock();
             OutputModule.Clock();
         }
 
@@ -48,7 +33,6 @@ namespace DigitalElectronics.BenEater.Computers.ViewModels
         public void LoadRAM(byte[] data)
         {
             _computer.LoadRAM(data);
-            MemoryModule.Clock();
         }
 
         public class ClockModuleViewModel : ModuleViewModel
@@ -121,106 +105,6 @@ namespace DigitalElectronics.BenEater.Computers.ViewModels
             public event EventHandler? IsRunningChanged;
 
             public event EventHandler? ClockSpeedChanged;
-        }
-
-        public class ProgramCounterViewModel : ModuleViewModel
-        {
-            private readonly BE801Computer _computer;
-            
-            public ProgramCounterViewModel(BE801Computer computer)
-            {
-                _computer = computer ?? throw new ArgumentNullException(nameof(computer));
-            }
-
-            public BitArray State => _computer.ProbePC();
-
-            public override void Clock()
-            {
-                RaisePropertyChanged(nameof(State));
-            }
-        }
-
-        public class BusViewModel : ModuleViewModel
-        {
-            private readonly BE801Computer _computer;
-
-            public BusViewModel(BE801Computer computer)
-            {
-                _computer = computer ?? throw new ArgumentNullException(nameof(computer));
-            }
-
-            public BitArray? State => _computer.ProbeBus();
-
-            public override void Clock()
-            {
-                RaisePropertyChanged(nameof(State));
-            }
-        }
-
-        public class MemoryModuleViewModel : ModuleViewModel
-        {
-            private readonly BE801Computer _computer;
-
-            public MemoryModuleViewModel(BE801Computer computer)
-            {
-                _computer = computer ?? throw new ArgumentNullException(nameof(computer));
-            }
-
-            /// <summary>
-            /// Contents of Memory Address Register (MAR)
-            /// </summary>
-            public BitArray MAR => _computer.ProbeMAR();
-
-            /// <summary>
-            /// Contents of the 16-byte RAM
-            /// </summary>
-            public IList<BitArray> MemoryContents => _computer.ProbeRAM();
-
-            public override void Clock()
-            {
-                RaisePropertyChanged(nameof(MAR));
-                RaisePropertyChanged(nameof(MemoryContents));
-            }
-        }
-
-        public class ALUViewModel : ModuleViewModel
-        {
-            private readonly BE801Computer _computer;
-
-            public ALUViewModel(BE801Computer computer)
-            {
-                _computer = computer ?? throw new ArgumentNullException(nameof(computer));
-            }
-
-            public BitArray ARegister => _computer.ProbeARegister();
-
-            public BitArray ALU => _computer.ProbeALU();
-
-            public BitArray BRegister => _computer.ProbeBRegister();
-
-            public override void Clock()
-            {
-                RaisePropertyChanged(nameof(ARegister));
-                RaisePropertyChanged(nameof(ALU));
-                RaisePropertyChanged(nameof(BRegister));
-            }
-        }
-
-        public class InstrRegisterViewModel : ModuleViewModel
-        {
-            private readonly BE801Computer _computer;
-
-            public InstrRegisterViewModel(BE801Computer computer)
-            {
-                _computer = computer ?? throw new ArgumentNullException(nameof(computer));
-            }
-
-            public BitArray State => _computer.ProbeInstrRegister();
-
-            public override void Clock()
-            {
-                RaisePropertyChanged(nameof(State));
-            }
         }
 
         public class OutputModuleViewModel : ModuleViewModel
