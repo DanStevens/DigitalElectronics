d188fd1a32bf7cc13e43f311e010850512fadeaa
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2024-12-04 19:50:43 +0000

    Created Benchmarking project with a memory diagnostic benchmark that calls `Clock` multiple times on a NOP program.

---
+++ Benchmarking/BE801ComputerBenchmarks.cs
@@ -0,0 +1,30 @@
+﻿using DigitalElectronics.BenEater.Computers;
+
+namespace Benchmarking;
+
+[MemoryDiagnoser]
+public class BE801ComputerBenchmarks
+{
+    private static readonly byte[] ZeroProgram = Enumerable.Repeat((byte)0, 16).ToArray();
+    private BE801Computer _subject;
+
+    [IterationSetup]
+    public void Setup()
+    {
+        _subject = new BE801Computer();
+        _subject.LoadRAM(ZeroProgram);
+    }
+
+    [Benchmark]
+    [Arguments(1)]
+    [Arguments(10)]
+    [Arguments(100)]
+    [Arguments(1000)]
+    public void Clock(int clockCycles)
+    {
+        for (int i = 0; i < clockCycles; i++)
+        {
+            _subject.Clock();
+        }
+    }
+}
