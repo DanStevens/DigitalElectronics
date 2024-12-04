666ae0cd5ca984dd00eb4a43dec79efcb39e6ba7
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2024-12-04 19:50:43 +0000

    Created Benchmarking project with a memory diagnostic benchmark that calls `Clock` multiple times on a NOP program.

---
+++ Benchmarking/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using Benchmarking;
+
+#if RELEASE
+
+BenchmarkRunner.Run<BE801ComputerBenchmarks>();
+
+#else
+
+var benchmark = new BE801ComputerBenchmarks();
+benchmark.Setup();
+benchmark.Clock(1);
+
+#endif
