480c09f3ce1e2bfc48263784c2748e31a8c1aea0
Dan Stevens <1074238+DanStevens@users.noreply.github.com>
2024-12-11 15:33:47 +0000

    Added additional benchmarks for creating BitArray instances
    Also renamed `BitArrayBenchmark` to `BitArrayInstantiationBenchmarks`, so the individual benchmarks can have shorter names
    
    ```
    | Method                                    | Mean       | Error     | StdDev    | Gen0   | Allocated |
    |------------------------------------------ |-----------:|----------:|----------:|-------:|----------:|
    | Default                                   |   4.102 ns | 0.0110 ns | 0.0097 ns |      - |         - |
    | FromArrayOf32Bools                        |  19.850 ns | 0.3102 ns | 0.2750 ns |      - |         - |
    | FromSpanOf32Bools                         |  20.006 ns | 0.2482 ns | 0.2322 ns |      - |         - |
    | FromEnumerableOf32Bools                   | 191.485 ns | 0.5158 ns | 0.4573 ns | 0.0057 |      48 B |
    | FromEnumerableOf32BoolsAndLength          | 185.994 ns | 0.6397 ns | 0.5984 ns | 0.0057 |      48 B |
    | FromCollectionOf32Bools                   | 190.623 ns | 0.6058 ns | 0.5667 ns | 0.0057 |      48 B |
    | FromListOf32Bools                         | 100.507 ns | 0.1856 ns | 0.1646 ns | 0.0057 |      48 B |
    | FromObservableCollectionOf32Bools         | 117.821 ns | 0.2864 ns | 0.2539 ns | 0.0057 |      48 B |
    | FromArrayOf4Bytes                         |   5.722 ns | 0.0094 ns | 0.0088 ns |      - |         - |
    | FromArrayOf32Bits                         |  92.669 ns | 0.2778 ns | 0.2599 ns | 0.0057 |      48 B |
    | FromEnumerableOf32Bits                    |  93.964 ns | 0.2430 ns | 0.2154 ns | 0.0057 |      48 B |
    | FromEnumerableOf32BitsAndLength           |  95.019 ns | 0.2345 ns | 0.2193 ns | 0.0057 |      48 B |
    | FromEnumerableOf32Bits_UsingSelect        |  93.283 ns | 0.2477 ns | 0.2196 ns | 0.0057 |      48 B |
    | FromCollectionOf32Bits                    | 101.300 ns |   1.26 ns | 0.9900 ns | 0.0057 |      48 B |
    | FromListOf32Bits                          | 193.678 ns | 0.5378 ns | 0.5031 ns | 0.0153 |     128 B |
    | FromListOf32Bits_UsingSelect              | 192.646 ns | 0.7743 ns | 0.7242 ns | 0.0153 |     128 B |
    | FromListOf32Bits_UsingSelectAndLength     | 190.268 ns | 0.5421 ns | 0.5071 ns | 0.0153 |     128 B |
    | FromArrayOf32Buffers_UsingSelect          |  94.009 ns | 0.2571 ns | 0.2147 ns | 0.0057 |      48 B |
    | FromArrayOf32Buffers_UsingSelectAndLength |  93.772 ns | 0.2286 ns | 0.2139 ns | 0.0057 |      48 B |
    ```

---
+++ Benchmarking/BitArrayInstantiationBenchmarks.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Specialized;
+using BenchmarkDotNet.Engines;
+using DigitalElectronics.Concepts;
+using DigitalElectronics.Utilities;
+using DigitalElectronics.Components.LogicGates;
+using Buffer = DigitalElectronics.Components.LogicGates.Buffer;
+using Microsoft.CodeAnalysis.Operations;
+using System.Collections.ObjectModel;
+
+namespace Benchmarking;
+
+[MemoryDiagnoser]
+//[IterationTime(10)]
+//[SimpleJob(launchCount: 1, warmupCount: 1, iterationCount: 1)]
+public class BitArrayInstantiationBenchmarks
+{
+    private static readonly bool[] ArrayOf32Bools =
+    {
+        false, true, false, true, false, true, false, true,
+        false, true, false, true, false, true, false, true,
+        false, true, false, true, false, true, false, true,
+        false, true, false, true, false, true, false, true
+    };
+
+    private static readonly List<bool> ListOf32Bools = new List<bool>(ArrayOf32Bools);
+    
+    private static readonly ObservableCollection<bool> ObservableCollectionOf32Bools = new ObservableCollection<bool>(ArrayOf32Bools.AsEnumerable());
+
+    private static readonly IEnumerable<Bit> EnumerableOf32Bits = ArrayOf32Bools.Select(b => new Bit(b));
+
+    private static readonly Bit[] ArrayOf32Bits = EnumerableOf32Bits.ToArray();
+
+    private static readonly IList<Bit> ListOf32Bits = new List<Bit>(EnumerableOf32Bits);
+
+    private static readonly ObservableCollection<Bit> ObservableListOf32Bits = new ObservableCollection<Bit>(EnumerableOf32Bits);
+
+    private static readonly byte[] ArrayOf4Bytes = { 0xAA, 0xBB, 0xCC, 0xDD };
+
+    private static readonly Buffer[] ArrayOf32Buffers = Enumerable.Range(0, 32).Select(_ => new Buffer()).ToArray();
+
+    [Benchmark]
+    public BitArray Default() => new BitArray();
+
+    [Benchmark]
+    public BitArray FromArrayOf32Bools() => new BitArray(ArrayOf32Bools);
+
+    [Benchmark]
+    public BitArray FromSpanOf32Bools() => new BitArray(ArrayOf32Bools.AsSpan());
+
+    [Benchmark]
+    public BitArray FromEnumerableOf32Bools() => new BitArray(ArrayOf32Bools.AsEnumerable());
+
+    [Benchmark]
+    public BitArray FromEnumerableOf32BoolsAndLength() => new BitArray(ArrayOf32Bools.AsEnumerable(), 32);
+
+    [Benchmark]
+    public BitArray FromCollectionOf32Bools() => new BitArray((ICollection<bool>)ArrayOf32Bools);
+
+    [Benchmark]
+    public BitArray FromListOf32Bools() => new BitArray(ListOf32Bools);
+
+    [Benchmark]
+    public BitArray FromObservableCollectionOf32Bools() => new BitArray(ObservableCollectionOf32Bools);
+
+    [Benchmark]
+    public BitArray FromArrayOf4Bytes() => new BitArray(ArrayOf4Bytes);
+
+    [Benchmark]
+    public BitArray FromArrayOf32Bits() => ArrayOf32Bits.ToBitArray();
+
+    [Benchmark]
+    public BitArray FromEnumerableOf32Bits() => ArrayOf32Bits.AsEnumerable().ToBitArray();
+
+    [Benchmark]
+    public BitArray FromEnumerableOf32BitsAndLength() => ArrayOf32Bits.AsEnumerable().ToBitArray(32);
+
+    [Benchmark]
+    public BitArray FromEnumerableOf32Bits_UsingSelect() => new BitArray(ArrayOf32Bits.Select(b => b.Value));
+
+    [Benchmark]
+    public BitArray FromCollectionOf32Bits() => ((ICollection<Bit>)ArrayOf32Bits).ToBitArray();
+
+    [Benchmark]
+    public BitArray FromListOf32Bits() => ListOf32Bits.ToBitArray();
+
+    [Benchmark]
+    public BitArray FromListOf32Bits_UsingSelect() => new BitArray(ListOf32Bits.Select(b => b.Value));
+
+    [Benchmark]
+    public BitArray FromListOf32Bits_UsingSelectAndLength() => new BitArray(ListOf32Bits.Select(b => b.Value), ListOf32Bits.Count);
+
+    //[Benchmark]
+    //public BitArray FromArrayOf32Buffers() => ArrayOf32Buffers.ToBitArray();
+
+    [Benchmark]
+    public BitArray FromArrayOf32Buffers_UsingSelect() => new BitArray(ArrayOf32Buffers.Select(b => b.OutputQ));
+
+    [Benchmark]
+    public BitArray FromArrayOf32Buffers_UsingSelectAndLength() => new BitArray(ArrayOf32Buffers.Select(b => b.OutputQ), ArrayOf32Buffers.Length);
+}
--- Benchmarking/Program.cs
+++ Benchmarking/Program.cs
@@ -1,10 +1,26 @@
-﻿using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
 using Benchmarking;
+using Perfolizer.Horology;
 
 #if RELEASE
 
+var quickJob = Job.Default
+    .WithIterationTime(new TimeInterval(10, TimeUnit.Millisecond))
+    .WithIterationCount(1)
+    .WithWarmupCount(1)
+    .WithIterationCount(1);
+
+var config = DefaultConfig.Instance
+    .AddJob(Job.Default)
+    //.AddJob(quickJob.WithRuntime(CoreRuntime.Core80))
+    //.AddJob(quickJob.WithRuntime(CoreRuntime.Core90))
+;
+
 //BenchmarkRunner.Run<BE801ComputerBenchmarks>();
-BenchmarkRunner.Run<BitArrayBenchmarks>();
+BenchmarkRunner.Run<BitArrayInstantiationBenchmarks>(config);
 
 #else
 
